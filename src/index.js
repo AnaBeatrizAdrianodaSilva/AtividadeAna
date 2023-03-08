d0efeb3aa580321e34fcb12ed73276dbd61890c6
Ana Beatriz <ana.github0@gmail.com>
2023-03-08 16:42:49 -0300

    Deu Certo!!!!!!!!!!!!!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Tab = createMaterialBottomTabNavigator();
+
+import HomeScreen from "./screens/HomeScreen";
+import AboutScreen from './screens/AboutScreen';
+import ContactScreen from './screens/ContactScreen';
 
 export default function RootNavigation() {
   return (
 
-    <Tab.Navigator>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="HomeScreen" component={HomeScreen} />
 
-      <Tab.Screen name="AboutScreen" component={AboutScreen} />
+        <Tab.Screen name="AboutScreen" component={AboutScreen} />
 
-      <Tab.Screen name="ContactScreen" component={ContactScreen} />
-    </Tab.Navigator>
+        <Tab.Screen name="ContactScreen" component={ContactScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+
+
 
   )
 }
